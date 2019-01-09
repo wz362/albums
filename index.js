3b6254c966d0742839c16934bd46be8d52423c31
Wenjia Zhang <wenjiazhang@Shaheers-MacBook-Pro.local>
2019-01-09 13:42:20 -0500

    Initial Setup including ESLint

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-/** @format */
+// Index.ios.js -  place code in here for IOS
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native'; /* Imports Destructuring */
 
-AppRegistry.registerComponent(appName, () => App);
+// Create a compenent  /* One Compoenent per file */
+const App = () => (
+    <Text>Some Text</Text> 
+    /* JSX: an extension to Javascript language that is used to write 
+            re-act components
+    */
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
+/*                              |           |
+                                |           |
+                            project name    |
+                                            |
+                                function that return the first 
+                                component to render in the 
+                                application
+    
+Note: For every react native app, we have to register at least one component.
+*/
